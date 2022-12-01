3bdff8fd346a5f4963e138e488786ec71198c666
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2022-12-02 00:34:58 +0100

    Download riddle, convert it to markdown, and save

--- aoc-prepare-problem.py
+++ aoc-prepare-problem.py
@@ -1,4 +1,8 @@
+from bs4 import BeautifulSoup
+from markdownify import markdownify as md
+from os import path
 import argparse
+import os
 import urllib.request
 
 BASE_URL="https://adventofcode.com"
@@ -14,15 +18,38 @@ def get_cmdline_args():
     return parser.parse_args()
 
 
-def main():
-    args = get_cmdline_args()
-    riddle_url = f"{BASE_URL}/{YEAR}/day/{args.day}"
+def scrap_riddle(day_number):
+    riddle_url = f"{BASE_URL}/{YEAR}/day/{day_number}"
     try:
         with urllib.request.urlopen(riddle_url) as ptr:
             page_bytes = ptr.read()
-            # TODO : Retrieve only the riddle from the webpage.
+            dom = BeautifulSoup(page_bytes.decode("utf8"), "html.parser")
+            riddle_content = dom.body.main.article
+            # Reformat the title : remove the '---' with is translated as a MD comment, and increase the title size.
+            title = str(riddle_content.h2.string)
+            riddle_content.h2.string.replace_with(title.replace("---", ""))
+            riddle_content.h2.name = 'h1'
+            return str(riddle_content)
     except urllib.error.URLError as e: 
         print(f"Unable to open the AdventOfCode riddle ({riddle_url}): {e}")
+        return ""
+
+
+def main():
+    args = get_cmdline_args()
+    print(f"Scraping the riddle of day {args.day} from the website")
+    riddle_html = scrap_riddle(args.day)
+    if len(riddle_html) == 0:
+        return
+    print("Converting the riddle in markdown")
+    riddle_md = md(riddle_html)
+    print("Creating the solution skeleton")
+    dirname = f"day{args.day}"
+    if not path.exists(dirname):
+        os.mkdir(dirname)
+    with open(path.join(dirname, "problem.md"), "w") as problem_file:
+        problem_file.write(riddle_md)
+    
 
     
 if __name__ == "__main__":
