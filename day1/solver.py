b4243d0dc3cb0576f1d9a7b54c1abdf8b7f15aaf
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2022-12-02 01:31:00 +0100

    Solve day 1 part 2

--- day1/solver.py
+++ day1/solver.py
@@ -1,6 +1,6 @@
 INPUT_FILE = "input.txt"
 
-def solve(input):
+def sort_elves_harvests_desc(input):
     harvest_per_elves = input.split("\n\n")
     total_per_elves = []
     for elf_harvest in harvest_per_elves:
@@ -11,15 +11,25 @@ def solve(input):
                 total += int(qty)
         total_per_elves.append(total)
     total_per_elves.sort(reverse=True)
+    return total_per_elves
+
+def solve1(input):
+    total_per_elves = sort_elves_harvests_desc(input)
     return total_per_elves[0]
 
+
+def solve2(input):
+    total_per_elves = sort_elves_harvests_desc(input)
+    return total_per_elves[0] + total_per_elves[1] + total_per_elves[2]
 
 def main():
     input = ""
     with open(INPUT_FILE, "r") as input_fd:
         input = input_fd.read()
-    result = solve(input)
-    print(f"Solution is : {result}")
+    result1 = solve1(input)
+    result2 = solve2(input)
+    print(f"Solution for part 1 is : {result1}")
+    print(f"Solution for part 2 is : {result2}")
 
 
 if __name__ == "__main__":
--- template-solver.py
+++ template-solver.py
@@ -1,17 +1,23 @@
 INPUT_FILE="input.txt"
 
 
-def solve(input):
+def solve1(input):
     # TODO: Code here
     pass
 
+
+def solve2(input):
+    # TODO: Code here
+    pass
 
 def main():
     input = ""
     with open(INPUT_FILE, "r") as input_fd:
         input = input_fd.read()
-    result = solve(input)
-    print(f"Solution is : {result}")
+    result1 = solve1(input)
+    result2 = solve2(input)
+    print(f"Solution for part 1 is : {result1}")
+    print(f"Solution for part 2 is : {result2}")
 
 
 if __name__ == "__main__":
